3b8f49a3691fd0a419cf051de46cefdfd7a11d0b
sever29 <samokhinda@gmail.com>
2024-09-11 11:41:24 +0300

    tm2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
 // next.config.js
 // const nextConfig = {
@@ -20,9 +20,9 @@
 // module.exports = nextConfig
 
 // next.config.js
-const nextConfig = {
-    reactStrictMode: true,
-    // Удалено exportPathMap
-}
+// const nextConfig = {
+//     reactStrictMode: true,
+//     // Удалено exportPathMap
+// }
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
