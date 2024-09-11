d6cf7dc872bee770b2c0b04f78340f1b2619d65c
sever29 <samokhinda@gmail.com>
2024-09-11 22:13:59 +0300

    woo1

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,21 +1,21 @@
-// import {NextRequest, NextResponse} from "next/server";
-// import woo from "@/lib/woo";
+import {NextRequest, NextResponse} from "next/server";
+import woo from "@/lib/woo";
 
-// export async function GET(request: NextRequest) {
-//     const params = request.nextUrl.searchParams
-//     params.set('status', 'publish')
-//     const res = await woo.get('products', params)
-//     return NextResponse.json(await res.json())
-// }
+export async function GET(request: NextRequest) {
+    const params = request.nextUrl.searchParams
+    params.set('status', 'publish')
+    const res = await woo.get('products', params)
+    return NextResponse.json(await res.json())
+}
 
 // /api/products/route.ts
 
-import { NextRequest, NextResponse } from 'next/server';
-import { get as getOpenCart } from '@/lib/opencart';
+// import { NextRequest, NextResponse } from 'next/server';
+// import { get as getOpenCart } from '@/lib/opencart';
 
-export async function GET(request: NextRequest) {
-    const params = request.nextUrl.searchParams;
-    params.set('status', 'publish');
-    const res = await getOpenCart('api/product/getProducts', params);
-    return NextResponse.json(await res.json());
-}
+// export async function GET(request: NextRequest) {
+//     const params = request.nextUrl.searchParams;
+//     params.set('status', 'publish');
+//     const res = await getOpenCart('api/product/getProducts', params);
+//     return NextResponse.json(await res.json());
+// }
