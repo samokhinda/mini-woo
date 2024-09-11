ab66835a018ebab263d02852ae9a5033ec8dfdf5
sever29 <samokhinda@gmail.com>
2024-09-11 10:31:44 +0300

    timeweb

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            // добавьте другие страницы, если они есть
+        }
+    }
+}
