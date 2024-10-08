da8a63e32d71963b062ffe46a69039983d353705
sever29 <samokhinda@gmail.com>
2024-10-08 19:19:21 +0300

    заказ успешно размещен

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,19 +32,22 @@ export default function Home() {
       const result = await res.json()
 
       if (invoiceSupported) {
-        webApp?.openInvoice(result.invoice_link, function (status) {
-          webApp?.MainButton.hideProgress()
-          if (status == 'paid') {
-            console.log("[paid] InvoiceStatus " + result);
-            webApp?.close();
-          } else if (status == 'failed') {
-            console.log("[failed] InvoiceStatus " + result);
-            webApp?.HapticFeedback.notificationOccurred('error');
-          } else {
-            console.log("[unknown] InvoiceStatus" + result);
-            webApp?.HapticFeedback.notificationOccurred('warning');
-          }
-        });
+        // webApp?.openInvoice(result.invoice_link, function (status) {
+        //   webApp?.MainButton.hideProgress()
+        //   if (status == 'paid') {
+        //     console.log("[paid] InvoiceStatus " + result);
+        //     webApp?.close();
+        //   } else if (status == 'failed') {
+        //     console.log("[failed] InvoiceStatus " + result);
+        //     webApp?.HapticFeedback.notificationOccurred('error');
+        //   } else {
+        //     console.log("[unknown] InvoiceStatus" + result);
+        //     webApp?.HapticFeedback.notificationOccurred('warning');
+        //   }
+        // });
+        webApp?.MainButton.hideProgress();
+        webApp?.showAlert("Ваш заказ успешно размещен!");
+        webApp?.close();        
       } else {
         webApp?.showAlert("Some features not available. Please update your telegram app!")
       }
